2dd27d55e9d50de1f5376698ca18af4e9877e8cb
Ellen102 <eavhove@gmail.com>
2025-02-08 20:05:31 +0100

    merge double branching on highest level

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -17,12 +17,22 @@ class GildedRose {
     }
 
     private static void updateQuality(Item item) {
-        if (item.name.equals(AGED_BRIE)) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
+        switch (item.name) {
+            case SULFURAS -> {
+
             }
-        } else {
-            if (item.name.equals(BACKSTAGE_PASSES)) {
+            case AGED_BRIE -> {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+
+                if (item.sellIn <= 0) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
+            case BACKSTAGE_PASSES -> {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
@@ -39,34 +49,23 @@ class GildedRose {
                     }
 
                 }
-            } else {
+
+                if (item.sellIn <= 0) {
+                    item.quality = 0;
+                }
+            }
+            default -> {
                 if (item.quality > 0) {
-                    if (!item.name.equals(SULFURAS)) {
-                        item.quality = item.quality - 1;
-                    }
+                    item.quality = item.quality - 1;
                 }
-            }
-        }
-
-        if (item.sellIn <= 0) {
-            if (item.name.equals(AGED_BRIE)) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            } else {
-                if (!item.name.equals(BACKSTAGE_PASSES)) {
+                if (item.sellIn <= 0) {
                     if (item.quality > 0) {
-                        if (!item.name.equals(SULFURAS)) {
-                            item.quality = item.quality - 1;
-                        }
+                        item.quality = item.quality - 1;
+
                     }
-                } else {
-                    item.quality = item.quality - item.quality;
                 }
             }
         }
-
-
 
 
     }
