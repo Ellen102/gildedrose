31da7a80191bf84c68ab7be5d57eb0a3ff0f2ed3
Ellen102 <eavhove@gmail.com>
2025-02-08 17:10:15 +0100

    update setup

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,18 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item item = new Item("foo", 0, 0);
+        Item[] items = new Item[] {item};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        assertThat(item.name).isEqualTo("foo");
     }
 
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Ellens solution to Gilded Rose
+
+## Changes to setup
+- [x] update to java 21
+- [x] add assertj
+  - Make tests easier to read and it is easier to understand failures
+- [ ] update gradle
+
+## Tests
